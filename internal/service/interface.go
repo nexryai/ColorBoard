ef6c3a78e84d09d468c8a45810b61db83db66329
nexryai <nexryai@sda1.net>
2024-07-17 15:09:44 +0900

    Add interfaces

---
+++ internal/service/interface.go
@@ -0,0 +1,9 @@
+package service
+
+type (
+	IUserService      interface{}
+	IAuthService      interface{}
+	IIdentService     interface{}
+	IThumbnailService interface{}
+	IStorageService   interface{}
+)
