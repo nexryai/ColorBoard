16bcb3d55df93993db4e5efb0713fbb755544d40
nexryai <nexryai@sda1.net>
2024-07-29 15:02:09 +0900

    Add interfaces

--- internal/service/interface.go
+++ internal/service/interface.go
@@ -1,9 +1,24 @@
 package service
 
+import (
+	"github.com/nexryai/ColorBoard/db"
+	"io"
+)
+
 type (
-	IUserService      interface{}
-	IAuthService      interface{}
-	IIdentService     interface{}
-	IThumbnailService interface{}
-	IStorageService   interface{}
+	IUserService    interface{}
+	IAuthService    interface{}
+	IIdentService   interface{}
+	IGalleryService interface {
+		CreateGallery(gallery db.GalleryModel) (string, error)
+		AddImage(reader io.Reader, gallery db.GalleryModel) (string, error)
+	}
+	IThumbnailService interface {
+		GenerateThumbnail(reader io.Reader) (*[]byte, error)
+	}
+	IStorageService interface {
+		CreateFile(reader io.Reader) (string, error)
+		GetFilePrivateUrl(id string) (string, error)
+		DeleteFile(id string) error
+	}
 )
--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/nexryai/ColorBoard/db"
+import (
+	"context"
+	"github.com/nexryai/ColorBoard/db"
+)
 
 func main() {
 	client := db.NewClient()
 	client.Connect()
+
+	user, err := client.User.FindFirst(db.User.ID.Equals("1")).Exec(context.TODO())
+	if user == nil {
+		println("User not found")
+	}
+
+	println(user.ID, err)
 }
