a2ec967339c3c90554a742599aae0391f4a8a002
nexryai <nexryai@sda1.net>
2024-08-01 08:26:11 +0900

    Client: Add galleries router

---
+++ client/src/routes/galleries/[id]/+page.ts
@@ -0,0 +1,12 @@
+/** @type {import('./$types').PageLoad} */
+import type { PageLoad } from "../../../../.svelte-kit/types/src/routes/galleries/[id]/$types"
+
+export const prerender = false
+export const ssr = false
+
+export const load: PageLoad = ({ params }) => {
+    return {
+        id: params.id
+    };
+};
+
