7ebc730fc8b83e353b0b08990e6a8cc5f6d6ba85
nexryai <nexryai@sda1.net>
2024-08-11 05:09:34 +0000

    Client: Fix router

---
+++ client/src/routes/galleries/[id]/upload/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { PageData } from "../../../../../.svelte-kit/types/src/routes/galleries/[id]/$types"
+    export let data: PageData
+    const galleryId = data.id
+</script>
+<div>
+    <p>{galleryId}</p>
+</div>
--- client/src/routes/galleries/[id]/upload/+page.ts
+++ client/src/routes/galleries/[id]/upload/+page.ts
@@ -1,5 +1,5 @@
 /** @type {import('./$types').PageLoad} */
-import type { PageLoad } from "../../../../.svelte-kit/types/src/routes/galleries/[id]/upload/$types"
+import type { PageLoad } from "../../../../../.svelte-kit/types/src/routes/galleries/[id]/upload/$types"
 
 export const prerender = false
 export const ssr = false
