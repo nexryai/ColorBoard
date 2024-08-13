65f37f4d740f4a55a170961dcc19f46b69467dee
nexryai <nexryai@sda1.net>
2024-08-13 00:41:19 +0000

    Add header

--- client/src/routes/galleries/[id]/+page.svelte
+++ client/src/routes/galleries/[id]/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+    import { afterUpdate } from "svelte"
     import type { PageData } from "../../../../.svelte-kit/types/src/routes/galleries/[id]/$types"
     import init, { render_blurhash } from "$lib/wasm/cb_client_wasm"
     import { fetchGallery } from "$lib/api"
-    import { afterUpdate } from "svelte"
+    import { Button } from "$lib/components/ui/button"
+    import CloudUpload from "@tabler/icons-svelte/icons/cloud-upload"
 
     export let data: PageData
     const galleryId = data.id
@@ -64,7 +66,13 @@
 </script>
 
 <div>
-    <p>{galleryId}</p>
+    <div class="flex justify-between items-center mb-8">
+        <p class="text-2xl">Galleries</p>
+        <Button variant="outline">
+            <CloudUpload class="mr-2" />
+            Upload
+        </Button>
+    </div>
     <div class="grid gap-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-5 w-[100%]" class:hidden={!placeholdersAreReady}>
         {#each placeholders as placeholder}
             <div class="w-[150px] h-[150px] overflow-hidden">
