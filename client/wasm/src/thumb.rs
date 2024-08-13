1a655da9006aca91b770bef47d132aed001bd7d3
nexryai <nexryai@sda1.net>
2024-08-13 01:44:49 +0000

    アップロード時に画像サイズを提供するように

--- client/wasm/src/thumb.rs
+++ client/wasm/src/thumb.rs
@@ -1,17 +1,13 @@
 use image::*;
 use std::io::Cursor;
 
-pub fn generate_thumbnail(data: &Vec<u8>) -> Vec<u8> {
-    // Using `image` crate, open the included .jpg file
-    let img = image::load_from_memory(data).unwrap();
-    let (w, h) = img.dimensions();
-
+pub fn generate_thumbnail(data: &DynamicImage, w: u32, h: u32) -> Vec<u8> {
     // 720pのサイズにリサイズ
     // 縦と横、超過が大きい方に合わせる
     let size_factor = 360.0 / (w.max(h) as f64);
 
     let img: DynamicImage = image::DynamicImage::ImageRgba8(imageops::resize(
-        &img,
+        data,
         (w as f64 * size_factor) as u32,
         (h as f64 * size_factor) as u32,
         imageops::FilterType::Triangle,
--- client/wasm/src/upload.rs
+++ client/wasm/src/upload.rs
@@ -1,10 +1,11 @@
 use std::io::Cursor;
+use image::GenericImageView;
 use uuid::Uuid;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 use ehttp::multipart::MultipartBuilder;
 
-use crate::{blurhash::get_blurhash, enc::encode_to_webp_lossless, thumb::generate_thumbnail};
+use crate::{blurhash::get_blurhash, thumb::generate_thumbnail};
 
 
 #[wasm_bindgen]
@@ -19,10 +20,12 @@ pub fn upload_file(gallery_id: String, data: Vec<u8>) -> u16 {
     // 一部の画像でサイズが異常に膨れ上がるので保留
     // log("[ColorBoard WASM] Encoding lossless WebP");
     // let lossless_data = encode_to_webp_lossless(&data);
+    let img = image::load_from_memory(&data).unwrap();
+    let (w, h) = img.dimensions();
     
     // Encode thumbnail
     log("[ColorBoard WASM] Encoding thumbnail WebP");
-    let thumbnail_data = generate_thumbnail(&data);
+    let thumbnail_data = generate_thumbnail(&img, w, h);
 
     let tmp_id = Uuid::new_v4();
     let filename = format!("{}.webp", tmp_id);
@@ -38,6 +41,8 @@ pub fn upload_file(gallery_id: String, data: Vec<u8>) -> u16 {
     let request = ehttp::Request::multipart(
         format!("/api/gallery/{}/upload", gallery_id),
         MultipartBuilder::new()
+            .add_text("width", &w.to_string())
+            .add_text("height", &h.to_string())
             .add_text("blurhash", &blurhash)
             .add_stream(
                 &mut Cursor::new(&data),
