175985e12cc942e89d527bf07a0c8b5603586fb2
nexryai <nexryai@sda1.net>
2024-08-13 06:10:35 +0000

    blurhashの計算方法を改善

--- client/wasm/src/blurhash.rs
+++ client/wasm/src/blurhash.rs
@@ -4,10 +4,24 @@ use image::*;
 use crate::log;
 
 pub fn get_blurhash(data: &Vec<u8>) -> String {
-    let image = image::load_from_memory(data).unwrap();
+    let mut image = image::load_from_memory(data).unwrap();
     let (width, height) = image.dimensions();
 
-    let result = encode(4, 3, width, height, &image.to_rgba8().into_vec());
+    // 正方形にトリミングする
+    let cropped_image = if width > height {
+        let x_offset = (width - height) / 2;
+        image.crop(x_offset, 0, height, height)
+    } else {
+        let y_offset = (height - width) / 2;
+        image.crop(0, y_offset, width, width)
+    };
+
+    let (new_width, new_height) = cropped_image.dimensions();
+
+    log(&format!("{}:{}", new_width, new_height));
+
+
+    let result = encode(4, 3, new_width, new_height, &cropped_image.to_rgba8().into_vec());
     let blurhash = match result {
         Ok(hash) => hash,
         Err(e) => {
