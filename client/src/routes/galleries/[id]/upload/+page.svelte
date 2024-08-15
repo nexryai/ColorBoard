731ca7fc2bd86e43903e8d74624615c02513e971
nexryai <nexryai@sda1.net>
2024-08-15 02:55:03 +0000

    アップロード時のクライアント側でのエラーハンドリングとキュー管理のリファクタリング

--- client/src/routes/galleries/[id]/upload/+page.svelte
+++ client/src/routes/galleries/[id]/upload/+page.svelte
@@ -1,25 +1,97 @@
 <script lang="ts">
     import type { PageData } from "../../../../../.svelte-kit/types/src/routes/galleries/[id]/$types"
     import Dropzone from "svelte-file-dropzone"
-    import { createUploadReader } from "$lib/upload"
+    import init, { upload_file } from "$lib/wasm/cb_client_wasm"
+
+    init().then(() => {
+        console.log("initialized")
+    })
 
     export let data: PageData;
     const galleryId = data.id;
 
-    let uploadQueue: { name: string }[] = [];
-    let failedFiles: { name: string }[] = [];
+    let uploadQueue: { name: string, done: boolean }[] = [];
+    let uploadedFiles: { name: string }[] = [];
+    let failedFiles: { name: string, reason?: string }[] = [];
+    
+    async function waitForDone(item: { done: boolean }) {
+        return new Promise<void>((resolve) => {
+            const checkDone = () => {
+                if (item.done) {
+                    resolve();
+                } else {
+                    // 100ミリ秒ごとにチェック
+                    setTimeout(checkDone, 100);
+                }
+            };
+            checkDone();
+        });
+    }
 
-    function handleFilesSelect(e: any) {
+    function createUploadReader(fileIndex: number) {
+        const reader = new FileReader()
+
+        reader.onload = async (e) => {
+            globalThis.isUploading = true
+            const filename = uploadQueue[fileIndex].name
+            let uploadRes: number
+
+            try {
+                // @ts-ignore
+                uploadRes = await upload_file(galleryId, new Uint8Array(reader.result))
+                console.log(`Upload resp: ${uploadRes}`)
+            } catch(e) {
+                failedFiles = [...failedFiles, {
+                    name: filename,
+                    reason: `Failed to process image: ${e}`
+                }];
+
+                return
+            } finally {
+                uploadQueue[fileIndex].done = true
+            }
+
+            if (uploadRes != 200) {
+                if (uploadRes == 1) {
+                    failedFiles = [...failedFiles, {
+                        name: filename,
+                        reason: "Failed to upload image."
+                    }];
+                } else {
+                    failedFiles = [...failedFiles, {
+                        name: filename,
+                        reason: `Server response code was not 200 (${uploadRes})`
+                    }];
+                }
+            } else {
+                uploadedFiles = [...uploadedFiles, {
+                    name: filename
+                }]
+            }
+        }
+
+        return reader
+    }
+
+    async function handleFilesSelect(e: any) {
         const { acceptedFiles, fileRejections } = e.detail;
-        console.log(e.detail);
-        uploadQueue = [...uploadQueue, ...acceptedFiles];
+        acceptedFiles.done = false
+
+        // Add ti queue
+        uploadQueue = [...acceptedFiles];
         failedFiles = [...failedFiles, ...fileRejections];
 
         console.log(uploadQueue);
         for (let i = 0; i < uploadQueue.length; i++) {
-            const reader = createUploadReader(galleryId);
+            const reader = createUploadReader(i);
             console.log(reader.readAsArrayBuffer(acceptedFiles[i]));
+
+            await waitForDone(uploadQueue[i]);
+            console.log(`Item ${i} is done.`);
         }
+
+        // リセットする
+        uploadQueue = []
     }
 </script>
 
@@ -29,6 +101,10 @@
     </div>
 
     {#each uploadQueue as item}
-        <li>{item}</li>
+        <li>{item.name}</li>
+    {/each}
+
+    {#each failedFiles as failed}
+        <li>{failed.name}: {failed.reason || "Failed to process image"}</li>
     {/each}
 </div>
