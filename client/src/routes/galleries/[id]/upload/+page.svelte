7a5816733839f6b8a59603fce9bb5832054b57c2
nexryai <nexryai@sda1.net>
2024-08-15 03:44:22 +0000

    Improve upload UI

--- client/src/routes/galleries/[id]/upload/+page.svelte
+++ client/src/routes/galleries/[id]/upload/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+    import { fly } from "svelte/transition"
     import type { PageData } from "../../../../../.svelte-kit/types/src/routes/galleries/[id]/$types"
     import Dropzone from "svelte-file-dropzone"
+    import LoaderCircle from "lucide-svelte/icons/loader-circle"
+    import PhotoX from "@tabler/icons-svelte/icons/photo-x"
+    import Check from "@tabler/icons-svelte/icons/check"
     import init, { upload_file } from "$lib/wasm/cb_client_wasm"
 
     init().then(() => {
@@ -101,15 +105,33 @@
 </script>
 
 <div>
+    <p>Queue</p>
     <div class="mt-8 mb-8">
         <Dropzone on:drop={handleFilesSelect} />
     </div>
 
     {#each uploadQueue as item}
-        <li>{item.name}</li>
+        {#if !item.done}
+            <div class="flex m-4 p-4 border border-gray-300 rounded-[10px]" in:fly="{{y: 70}}" out:fly="{{x: 70}}">
+                <LoaderCircle class="mt-[4px] mr-2 h-4 w-4 animate-spin" />
+                {item.name}
+            </div>
+        {/if}
     {/each}
 
+    <p class="mt-8">Errors</p>
     {#each failedFiles as failed}
-        <li>{failed.name}: {failed.reason || "Failed to process image"}</li>
+        <div class="text-orange-700 flex m-4 p-4 border border-orange-700 rounded-[10px]" in:fly="{{y: 70}}" out:fly="{{x: 70}}">
+            <PhotoX class="mt-[4px] mr-2 h-4 w-4" />
+            {failed.name}: {failed.reason || "Failed to process image"}
+        </div>
+    {/each}
+
+    <p class="mt-8">Uploaded files</p>
+    {#each uploadedFiles as upload}
+        <div class="text-green-700 flex m-4 p-4 border border-green-700 rounded-[10px]" in:fly="{{y: 70}}" out:fly="{{x: 70}}">
+            <Check class="mt-[4px] mr-2 h-4 w-4" />
+            {upload.name}
+        </div>
     {/each}
 </div>
